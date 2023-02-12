17e4ed4a965fba97b1df9e689ce3282103f3aab6
Abbas Atwi <abbasatwi@MacBook-Air-de-Abbas.local>
2023-02-12 02:47:57 -0300

    updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,4 @@
 let menuclick = document.querySelector(".menu-burguer");
 menuclick.addEventListener("click", () => {
   let menu = document.querySelector(".menu");
-  if ((menu.style.marginLeft = "-150px")) {
-    menu.style.marginLeft = "0px";
-  }
-
-  setInterval(() => {
-    menu.style.marginLeft = "-150px";
-  }, 3000);
 });
